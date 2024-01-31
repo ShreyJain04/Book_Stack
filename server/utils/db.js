fe8d6d2c1c2e14121e2fe447b62e8e81ee27a510
ShreyJain04 <jainshrey200349@gmail.com>
2024-02-01 00:42:02 +0530

    server deploy test 1

--- server/utils/db.js
+++ server/utils/db.js
@@ -2,10 +2,12 @@ const mongoose= require('mongoose');
 
 const URI= process.env.MONGODB_URI;
 
+
 const connectDB= async()=>{
     try{
+        console.log(MONGODB_URI);
         await mongoose.connect(URI);
-        console.log("Connection Successful to DB");
+        console.log("Connection Successful to DB");        
     }catch(err){
         console.error("DataBase Connection failed");
         process.exit(0);
